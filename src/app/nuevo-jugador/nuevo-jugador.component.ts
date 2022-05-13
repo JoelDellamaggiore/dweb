1b0b164d5207842484f4e343f0b5b426c879a929
JoelDellamaggiore <joeldellamaggiore@icloud.com>
2022-05-12 23:16:53 -0300

    Inicio DWEB
    Componente nuevo jugador

---
+++ src/app/nuevo-jugador/nuevo-jugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-jugador',
+  templateUrl: './nuevo-jugador.component.html',
+  styleUrls: ['./nuevo-jugador.component.css']
+})
+export class NuevoJugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
