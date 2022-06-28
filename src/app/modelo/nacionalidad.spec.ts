a016d09a00b6395b377a5ee94f028eb447ae40cb
JoelDellamaggiore <56844660+JoelDellamaggiore@users.noreply.github.com>
2022-06-28 16:41:17 -0300

    Se elimina test

--- src/app/modelo/nacionalidad.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Nacionalidad } from './nacionalidad';
-
-describe('Nacionalidad', () => {
-  it('should create an instance', () => {
-    expect(new Nacionalidad()).toBeTruthy();
-  });
-});
