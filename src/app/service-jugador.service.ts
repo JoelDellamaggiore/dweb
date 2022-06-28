7edca8e800709158c902165dfe960dac8d86e3e3
JoelDellamaggiore <56844660+JoelDellamaggiore@users.noreply.github.com>
2022-06-28 16:27:14 -0300

    Actualizacion de servicio
    Se implementa:
    -API_URL desde environment
    
    Se agregan los metodos:
    -actualizarJugador()
    -guardarJugador()
    -getFacultades()
    -getDisciplinas()
    -getNacionalidades()
    -obtenerJugador()
    -eliminarJugador()

--- src/app/service-jugador.service.ts
+++ src/app/service-jugador.service.ts
@@ -1,38 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Disciplina } from './modelo/disciplina';
+import { Facultad } from './modelo/facultad';
 import { Jugador } from './modelo/jugador';
-
+import { Nacionalidad } from './modelo/nacionalidad';
 @Injectable({
   providedIn: 'root',
 })
-//hay que crear una clase jugador
 export class ServiceJugadorService {
-  url:string = 'http://localhost:8080/api/jugadores';
+  private API_URL= environment.API_URL;
+  
   constructor(private http:HttpClient) {}
-  // jugador() {
-  //   return [
-  //     {
-  //       nombre: 'Joel',
-  //       apellido: 'Dellamaggiore',
-  //       dni: '42511013',
-  //       fechaNac: '2000-03-25',
-  //       facultad: 'Villa Maria',
-  //       disciplina: { nombre: 'Futbol', key: 'fut' },
-  //       nacionalidad: 'Argentina',
-  //     },
-  //     {
-  //       nombre: 'Santiago',
-  //       apellido: 'Giordano',
-  //       dni: '42337809',
-  //       fechaNac: '2000-01-16',
-  //       facultad: 'Villa Maria',
-  //       disciplina: { nombre: 'Voley', key: 'vol' },
-  //       nacionalidad: 'Argentina',
-  //     },
-  //   ];
-  // }
+  
   getJugadores(): Observable<Jugador[]>{
-    return this.http.get<Jugador[]>(this.url+ '/listar');
+    return this.http.get<Jugador[]>(this.API_URL+ 'listar');
+  }
+  actualizarJugador(jugador: Jugador): Observable<Jugador> {
+    return this.http.post<Jugador>(this.API_URL+'actualizar/'+jugador.legajo,jugador);
+  }
+  guardarJugador(jugador: Jugador): Observable<Jugador> {
+    return this.http.post<Jugador>(this.API_URL+'nuevo-jugador',jugador);
+  }
+  getFacultades(): Observable<Facultad[]>{
+    return this.http.get<Facultad[]>(this.API_URL+ 'facultades');
+  }
+  getDisciplinas(): Observable<Disciplina[]>{
+    return this.http.get<Disciplina[]>(this.API_URL+ 'disciplinas');
+  }
+  getNacionalidades(): Observable<Nacionalidad[]>{
+    return this.http.get<Nacionalidad[]>(this.API_URL+ 'nacionalidades');
+  }
+  obtenerJugador(legajo:string): Observable<Jugador>{
+    return this.http.get<Jugador>(this.API_URL+legajo);
+  }
+  eliminarJugador(legajo:string): Observable<any>{
+    return this.http.delete<any>(this.API_URL+'eliminar/'+legajo);
   }
 }
