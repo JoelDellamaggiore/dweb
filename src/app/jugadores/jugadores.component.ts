7ad6ff58455d7d6d244d8073a0d5565eb255189f
JoelDellamaggiore <56844660+JoelDellamaggiore@users.noreply.github.com>
2022-06-28 16:36:33 -0300

    Actualización de métodos y tabla de jugadores

--- src/app/jugadores/jugadores.component.ts
+++ src/app/jugadores/jugadores.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Jugador } from '../modelo/jugador';
-
+import Swal from 'sweetalert2';
 import { ServiceJugadorService } from '../service-jugador.service';
 
 @Component({
@@ -9,28 +9,39 @@ import { ServiceJugadorService } from '../service-jugador.service';
   styleUrls: ['./jugadores.component.css'],
 })
 export class JugadoresComponent implements OnInit {
-  // jugador: any;
-  // constructor(private jugadoresDatos: ServiceJugadorService) {
-  //   this.jugador = jugadoresDatos.jugador();
-  // }
-
-  // ngOnInit(): void {}
-  // filtros: any[] = [
-  //   { nombre: 'Facultad', key: 'fac' },
-  //   { nombre: 'Disciplina', key: 'dis' },
-  // ];
-
-  // editar(id: number) {
-  //   console.log(id);
-  // }
-  // eliminar(id: number) {
-  //   console.log(id);
-  // }
   jugador: Jugador[] = [];
   constructor(private jugadorservicio: ServiceJugadorService) {}
   ngOnInit(): void {
     this.jugadorservicio
       .getJugadores()
       .subscribe((response) => (this.jugador = response));
+  }
+  eliminarJugador(legajo:string){
+    Swal.fire({
+      title: 'Desea eliminar el jugador del legajo '+legajo+'?',
+      showDenyButton: true,
+      confirmButtonText: 'Si',
+      denyButtonText: 'No',
+      icon: 'question',
+      customClass: {
+        actions: 'my-actions',
+        cancelButton: 'order-1 right-gap',
+        confirmButton: 'order-2',
+        denyButton: 'order-3',
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.jugadorservicio
+        .eliminarJugador(legajo)
+        .subscribe(response => Swal.fire({
+          icon: 'success',
+          title: 'Jugador eliminado',
+          text: 'El jugador ha sido eliminado con exito ',
+        }));
+      } else if (result.isDenied) {
+        Swal.fire('El jugador no ha sido eliminado', '', 'info')
+      }
+    })
+    
   }
 }
