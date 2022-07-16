378bfddc2568e481f6b39427f5e234240ff63e83
wink15 <89814910+wink15@users.noreply.github.com>
2022-07-16 01:32:23 -0300

    Se agg servicio de disciplina

---
+++ src/app/service/disciplina.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Disciplina } from '../modelo/disciplina';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DisciplinaService {
+
+  url: string = 'http://localhost:8080/api/disciplina';
+
+  constructor( private http: HttpClient) { 
+     
+  }
+
+  getDisciplina(): Observable<Disciplina[]>{
+    return this.http.get<Disciplina[]>(this.url+'/listar');
+  }
+
+  crearDisciplina(disc: Disciplina): Observable<Disciplina>{
+    return this.http.post<Disciplina>(this.url + "/crear", disc);
+
+  }
+}
