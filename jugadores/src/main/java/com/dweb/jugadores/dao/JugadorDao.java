5f96a1f326b3c7142e84c1e09a0ae3a766211edb
wink15 <89814910+wink15@users.noreply.github.com>
2022-07-25 05:03:04 -0300

    Se agg filtro por nombre

--- jugadores/src/main/java/com/dweb/jugadores/dao/JugadorDao.java
+++ jugadores/src/main/java/com/dweb/jugadores/dao/JugadorDao.java
@@ -1,9 +1,16 @@
 package com.dweb.jugadores.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dweb.jugadores.models.Jugador;
 
 public interface JugadorDao extends JpaRepository<Jugador, String>{
-    
+
+  @Query("SELECT j FROM jugador j WHERE " +
+  "Upper(j.nombre) LIKE CONCAT('%',:filtro, '%')"  )
+   List<Jugador> search(String filtro,String filtroDisc );
+
 }
